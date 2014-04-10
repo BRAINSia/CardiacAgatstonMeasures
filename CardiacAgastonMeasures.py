34bb5617b59cb0c0d2f11548957e9088ce86c2be
Jessica Forbes <jessica-forbes@uiowa.edu>
2014-04-10 16:54:33 -0500

    ENH: Sets up the widget class

--- CardiacAgastonMeasures.py
+++ CardiacAgastonMeasures.py
@@ -7,14 +7,34 @@ from __main__ import vtk, qt, ctk, slicer
 class CardiacAgastonMeasures:
     def __init__(self, parent):
         parent.title = "Cardiac Agaston Measures"
-        parent.categories = ["Cardiac Agastan Measures"]
+        parent.categories = ["Cardiac Agaston Measures"]
         parent.dependencies = []
         parent.contributors = ["Jessica Forbes (SINAPSE)",
                                "Hans Johnson (SINAPSE)"]
-                               parent.helpText = """
+        parent.helpText = """
                                    This module will auto-segment the calcium deposits in Cardiac CT scans.  The user selects calcium clusters that represent calcium plaques.  The user then selects calcium clusters that do not represent calcium plaques and that should not be included in the final calculations.  Slicer will take the mean, peak, variance, mass and threshold and put the values into an Excel (or Excel compatible) file.
                                    """
-                               parent.acknowledgementText = """
+        parent.acknowledgementText = """
                                    This file was originally developed by Jessica Forbes and Hans Johnson of the SINAPSE Lab at the University of Iowa.""" # replace with organization, grant and thanks.
-                               self.parent = parent
+        self.parent = parent
 
+#
+# CardiacAgastonMeasuresWidget
+#
+
+class CardiacAgastonMeasuresWidget:
+    def __init__(self, parent = None):
+        if not parent:
+            self.parent = slicer.qMRMLWidget()
+            self.parent.setLayout(qt.QVBoxLayout())
+            self.parent.setMRMLScene(slicer.mrmlScene)
+        else:
+            self.parent = parent
+        self.layout = self.parent.layout()
+        if not parent:
+            self.setup()
+            self.parent.show()
+
+    def setup(self):
+        # Instantiate and connect widgets ...
+        pass
