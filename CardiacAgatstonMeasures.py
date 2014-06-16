9c962ab3d507e69c8b8f4b935b54e31b3d0373a5
Jessica Forbes <jessica-forbes@uiowa.edu>
2014-06-16 16:33:27 -0500

    ENH: Adds test and self-test code created using ModuleWizard
    Added classes CardiacAgatstonMeasuresLogic and
    CardiacAgatstonMeasuresTest created using the ModuleWizard
    found at http://www.slicer.org/slicerWiki/index.php/
    Documentation/Nightly/Developers/ModuleWizard. This code
    adds a default self-test to the Slicer Testing->SelfTests
    options.  This is a requirement to add the module to the
    2014 NAM-IC Module Tutorial Contest. The default self-test
    will be adjusted to test the features of this module.

--- CardiacAgatstonMeasures.py
+++ CardiacAgatstonMeasures.py
@@ -1,4 +1,5 @@
 from __main__ import vtk, qt, ctk, slicer
+import unittest
 import os
 import SimpleITK as sitk
 import sitkUtils as su
@@ -23,6 +24,19 @@ class CardiacAgatstonMeasures:
         parent.acknowledgementText = """
                                    This file was originally developed by Jessica Forbes and Hans Johnson of the SINAPSE Lab at the University of Iowa.""" # replace with organization, grant and thanks.
         self.parent = parent
+
+        # Add this test to the SelfTest module's list for discovery when the module
+        # is created.  Since this module may be discovered before SelfTests itself,
+        # create the list if it doesn't already exist.
+        try:
+            slicer.selfTests
+        except AttributeError:
+            slicer.selfTests = {}
+        slicer.selfTests['CardiacAgatstonMeasures'] = self.runTest
+
+    def runTest(self):
+        tester = CardiacAgatstonMeasuresTest()
+        tester.runTest()
 
 #
 # CardiacAgatstonMeasuresWidget
@@ -286,7 +300,180 @@ class CardiacAgatstonMeasuresWidget:
         setattr(globals()['slicer'].modules, widgetName, globals()[widgetName.lower()])
 
     def onReloadAndTest(self,moduleName="CardiacAgatstonMeasures"):
-        print "onReloadAndTest running"
+        try:
+            self.onReload()
+            evalString = 'globals()["%s"].%sTest()' % (moduleName, moduleName)
+            tester = eval(evalString)
+            tester.runTest()
+        except Exception, e:
+            import traceback
+            traceback.print_exc()
+            qt.QMessageBox.warning(slicer.util.mainWindow(),
+              "Reload and Test", 'Exception!\n\n' + str(e) + "\n\nSee Python Console for Stack Trace")
+
+#
+# CardiacAgatstonMeasuresLogic
+#
+
+class CardiacAgatstonMeasuresLogic:
+    """This class should implement all the actual
+    computation done by your module.  The interface
+    should be such that other python code can import
+    this class and make use of the functionality without
+    requiring an instance of the Widget
+    """
+    def __init__(self):
+        pass
+
+    def hasImageData(self,volumeNode):
+        """This is a dummy logic method that
+        returns true if the passed in volume
+        node has valid image data
+        """
+        if not volumeNode:
+            print('no volume node')
+            return False
+        if volumeNode.GetImageData() == None:
+            print('no image data')
+            return False
+        return True
+
+    def delayDisplay(self,message,msec=1000):
+        #
+        # logic version of delay display
+        #
+        print(message)
+        self.info = qt.QDialog()
+        self.infoLayout = qt.QVBoxLayout()
+        self.info.setLayout(self.infoLayout)
+        self.label = qt.QLabel(message,self.info)
+        self.infoLayout.addWidget(self.label)
+        qt.QTimer.singleShot(msec, self.info.close)
+        self.info.exec_()
+
+    def takeScreenshot(self,name,description,type=-1):
+        # show the message even if not taking a screen shot
+        self.delayDisplay(description)
+
+        if self.enableScreenshots == 0:
+            return
+
+        lm = slicer.app.layoutManager()
+        # switch on the type to get the requested window
+        widget = 0
+        if type == -1:
+            # full window
+            widget = slicer.util.mainWindow()
+        elif type == slicer.qMRMLScreenShotDialog().FullLayout:
+            # full layout
+            widget = lm.viewport()
+        elif type == slicer.qMRMLScreenShotDialog().ThreeD:
+            # just the 3D window
+            widget = lm.threeDWidget(0).threeDView()
+        elif type == slicer.qMRMLScreenShotDialog().Red:
+            # red slice window
+            widget = lm.sliceWidget("Red")
+        elif type == slicer.qMRMLScreenShotDialog().Yellow:
+            # yellow slice window
+            widget = lm.sliceWidget("Yellow")
+        elif type == slicer.qMRMLScreenShotDialog().Green:
+            # green slice window
+            widget = lm.sliceWidget("Green")
+
+        # grab and convert to vtk image data
+        qpixMap = qt.QPixmap().grabWidget(widget)
+        qimage = qpixMap.toImage()
+        imageData = vtk.vtkImageData()
+        slicer.qMRMLUtils().qImageToVtkImageData(qimage,imageData)
+
+        annotationLogic = slicer.modules.annotations.logic()
+        annotationLogic.CreateSnapShot(name, description, type, self.screenshotScaleFactor, imageData)
+
+    def run(self,inputVolume,outputVolume,enableScreenshots=0,screenshotScaleFactor=1):
+        """
+        Run the actual algorithm
+        """
+
+        self.delayDisplay('Running the aglorithm')
+
+        self.enableScreenshots = enableScreenshots
+        self.screenshotScaleFactor = screenshotScaleFactor
+
+        self.takeScreenshot('CardiacAgatstonMeasures-Start','Start',-1)
+
+        return True
+
+class CardiacAgatstonMeasuresTest(unittest.TestCase):
+    """
+    This is the test case for your scripted module.
+    """
+
+    def delayDisplay(self,message,msec=1000):
+        """This utility method displays a small dialog and waits.
+        This does two things: 1) it lets the event loop catch up
+        to the state of the test so that rendering and widget updates
+        have all taken place before the test continues and 2) it
+        shows the user/developer/tester the state of the test
+        so that we'll know when it breaks.
+        """
+        print(message)
+        self.info = qt.QDialog()
+        self.infoLayout = qt.QVBoxLayout()
+        self.info.setLayout(self.infoLayout)
+        self.label = qt.QLabel(message,self.info)
+        self.infoLayout.addWidget(self.label)
+        qt.QTimer.singleShot(msec, self.info.close)
+        self.info.exec_()
+
+    def setUp(self):
+        """ Do whatever is needed to reset the state - typically a scene clear will be enough.
+        """
+        slicer.mrmlScene.Clear(0)
+
+    def runTest(self):
+        """Run as few or as many tests as needed here.
+        """
+        self.setUp()
+        self.test_CardiacAgatstonMeasures1()
+
+    def test_CardiacAgatstonMeasures1(self):
+        """ Ideally you should have several levels of tests.  At the lowest level
+        tests sould exercise the functionality of the logic with different inputs
+        (both valid and invalid).  At higher levels your tests should emulate the
+        way the user would interact with your code and confirm that it still works
+        the way you intended.
+        One of the most important features of the tests is that it should alert other
+        developers when their changes will have an impact on the behavior of your
+        module.  For example, if a developer removes a feature that you depend on,
+        your test should break so they know that the feature is needed.
+        """
+
+        self.delayDisplay("Starting the test")
+        #
+        # first, get some data
+        #
+        import urllib
+        # downloads = (
+        #     ('http://slicer.kitware.com/midas3/download?items=5767', 'FA.nrrd', slicer.util.loadVolume),
+        #     )
+        #
+        # for url,name,loader in downloads:
+        #   filePath = slicer.app.temporaryPath + '/' + name
+        #   if not os.path.exists(filePath) or os.stat(filePath).st_size == 0:
+        #     print('Requesting download %s from %s...\n' % (name, url))
+        #     urllib.urlretrieve(url, filePath)
+        #   if loader:
+        #     print('Loading %s...\n' % (name,))
+        #     loader(filePath)
+
+        slicer.util.loadVolume('/scratch/p1_1.nii.gz')  #added tmp because slicer.kitware.com download not working
+        self.delayDisplay('Finished with download and loading\n')
+
+        volumeNode = slicer.util.getNode(pattern="p1_1") #added tmp because slicer.kitware.com download not working
+        # volumeNode = slicer.util.getNode(pattern="FA")
+        logic = CardiacAgatstonMeasuresLogic()
+        self.assertTrue( logic.hasImageData(volumeNode) )
+        self.delayDisplay('Test passed!')
 
 class CardiacStatisticsWidget(LabelStatistics.LabelStatisticsWidget):
     def __init__(self, KEV120, KEV80, localCardiacEditorWidget, parent=None):
